fece734fda8654a3e9552eaed95ba46868b018f6
Bello Mercado Dev <dev@bellomercado.example.com>
2026-06-08 10:02:20 -0300

    Fix SyntaxError duplicate PORT

--- server.js
+++ server.js
@@ -10,8 +10,6 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(cors());
 app.use(express.json());
 
